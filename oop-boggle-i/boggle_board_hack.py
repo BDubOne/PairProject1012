ab22e5c0dc6b2d79e403b0e32b3d199d7bea54a8
Frank_Gary <blackrebelradio1992@gmail.com>
2023-10-12 14:43:25 -0500

    new method

--- oop-boggle-i/boggle_board_hack.py
+++ oop-boggle-i/boggle_board_hack.py
@@ -7,7 +7,7 @@ class BoggleBoard:
     # print("----\n----\n----\n----")
     pass
   
-  @staticmethod
+
   def shake():
     count = 0
     while count <= 4:
@@ -17,13 +17,12 @@ class BoggleBoard:
         print (my_string)
         count += 1
        
-  @staticmethod
   # generates random char
   def random_char(y):
        return ''.join(random.choice(string.ascii_uppercase) for x in range(y))
   
 
-  @staticmethod
+
   def start_game():
     #  print("----\n----\n----\n----")
     start = input("shake the board?(Y/n) ").upper()
@@ -32,33 +31,3 @@ class BoggleBoard:
       start = input("shake the board?(Y/n) ").upper()
     if start == "N":
       print("Bye")
-
-      
-  
-    # creates boggle board
-  # def generate_board():
-    # def random_char(y):
-    #    return ''.join(random.choice(string.ascii_uppercase) for x in range(y))
-    # count = 0
-    # while count <= 4:
-    #     print (random_char(5))
-    #     count += 1
-
-  
-
-
-
-# print (random_char(5))
-# print (random_char(5))
-# print (random_char(5))
-
-
-# random.choice(string.ascii_uppercase)
-
-
-  
-
-# print(random.choice(string.ascii_uppercase))
-# print("----\n----\n----\n----")
-
-BoggleBoard.start_game()
